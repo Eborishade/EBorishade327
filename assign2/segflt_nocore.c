5476c62fda77575715fc3317552dd1462a0fb749
Eborishade <eborishade@gmail.com>
2023-02-03 13:24:50 -0600

    seg fault (no core)

---
+++ assign2/segflt_nocore.c
@@ -0,0 +1,44 @@
+/*
+Goal: Write a C program named segflt_core.c that will crash and cause a 'segfault'.
+ To cause the segfault, perform a memory access beyond what you have
+ allocated via malloc API. Before running your program, verify the resource limit
+ for core dump is non-zero by running ulimit -c. If the size is 0, set the core
+ file size to unlimited using the command: ulimit -c unlimited
+*/
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/timeb.h>
+#include <sys/resource.h>
+#include <errno.h>
+
+
+int main(int argc, char *argv[])
+{
+
+    struct rlimit resource_limit;
+    resource_limit.rlim_cur = 0;
+    resource_limit.rlim_max = RLIMIT_CORE;
+    
+    if (prlimit(0, RLIMIT_CORE, &resource_limit, 0) < 0){
+        //error occured if neg val
+        fprintf(stderr, "prlimit failed. errno=%d\n", errno); //global var errno
+        exit(EXIT_FAILURE);
+    }
+    
+    
+    char *ptr = NULL;
+    ptr = malloc(256 * sizeof(char));
+    if (!ptr)
+    {
+        fprintf(stderr, "Malloc Failed: Not Alloc-ed. \n"); // fprintf->anywhere else
+        exit(EXIT_FAILURE);
+    }
+
+    *(ptr + 100) = 'a'; //no seg fault
+    *(ptr + 500) = 'b'; //no seg fault
+    *(ptr + 5000000) = 'c'; //seg fault!!
+
+    printf("Size of char is %ld byte(s) \n ", sizeof(char)); // printf->stdout ALWAYS
+    free(ptr);
+}
