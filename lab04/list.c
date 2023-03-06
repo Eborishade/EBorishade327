b3e9dbc432798b897b1c31b46f6c606ea1e38854
Eborishade <eborishade@gmail.com>
2023-03-06 16:22:10 -0600

    lab4 (assign4 complete)

--- lab04/list.c
+++ lab04/list.c
@@ -15,18 +15,219 @@ LIST *list_create(){
     return new_list;
 }
 
+
 void list_append(LIST *list, void *element) {
-    if (list->head == NULL && list->tail == NULL) {
-        NODE *new_node = malloc(sizeof(NODE));
-        if (!new_node) {
-            perror("list_append");
-            exit(EXIT_FAILURE);
-        }
-        new_node->data = element;
+    NODE *new_node = malloc(sizeof(NODE));
+    if (!new_node) {
+        perror("list_append");
+        exit(EXIT_FAILURE);
+    }
+    new_node->data = element;
+
+    //if list is null
+    if (list->head == list->tail && list->head == NULL) {
         new_node->next = NULL; 
+
         list->head = new_node;
         list->tail = new_node;
     }
+    //if list has one or more element(s)
+    else {
+        list->tail->next = new_node; 
+        list->tail = new_node;
+    }
+}
 
+
+// Append the element at a particular location.
+// If location is beyond the end of the list, append the item at the end of the list.
+// Returns the location of the new element.
+unsigned int list_insert_at(LIST *list, void *element, unsigned int location){
+    NODE *prev = NULL;
+    NODE *current = list->head;
+    unsigned int i = 0;
+
+    //find location
+    while (current != NULL && i < location) {
+        prev = current;
+        current = current->next;
+        i++;
+    }
+
+    //create new node
+    NODE *new_node = malloc(sizeof(NODE));
+    if (!new_node) {
+        perror("list_insert_at");
+        exit(EXIT_FAILURE);
+    }
+    new_node->data = element;
+    new_node->next = current;
+
+    //INSERT
+
+    //if empty
+    if (prev == NULL) {
+        list->head = new_node;
+    } else {
+        prev->next = new_node;
+    }
+
+    //if last element
+    if (current == NULL) {
+        list->tail = new_node;
+    }
+
+    return i;
+}
+
+
+// Remove the element from the list
+void list_remove(LIST *list, void *element){
+    NODE *prev = NULL;
+    NODE *current = list->head;
+
+    //search using data
+    while (current != NULL && current->data != element) {
+        prev = current;
+        current = current->next;
+    }
+
+    //if found:
+    if (current != NULL) {
+
+        //if head
+        if (prev == NULL) {
+            list->head = current->next;
+        } else {
+            prev->next = current->next;
+        }
+
+        //if tail
+        if (current->next == NULL) {
+            list->tail = prev;
+        }
+
+        //finally, remove
+        free(current);
+
+    } //else do nothing
+}
+
+
+// Remove the element at location from the list
+// Returns a pointer to the element removed, null if there is no element at that location
+void *list_remove_at(LIST *list, unsigned int location){
+    //for [location] node = next. if NULL break, do nothing?
+    NODE *prev = NULL;
+    NODE *current = list->head;
+    unsigned int i = 0;
+
+    //find location
+    while (current != NULL && i < location) {
+        prev = current;
+        current = current->next;
+        i++;
+    }
+
+    //if not found
+    if (current == NULL) {
+        return NULL;
+    }
+
+    //UNLINK
+
+    //if removing first element
+    if (prev == NULL) {
+        list->head = current->next;
+    } else {
+        prev->next = current->next;
+    }
+
+    //if removing last element
+    if (current->next == NULL) {
+        list->tail = prev;
+    }
+
+    //get data & return
+    void *data = current->data;
+    free(current);
+
+    return data;
+}
+
+
+// Returns the list of element pointers as an array
+void *list_to_array(LIST *list){
+    int size = list_size(list);
+    void **arr = malloc(sizeof(void*) * size);
+    if(!arr){
+        perror("list_to_array");
+        exit(EXIT_FAILURE);
+    }
+
+    NODE *node = list->head;
+    int i = 0;
+    while (node != NULL) {
+        arr[i] = node->data;
+        i++;
+        node = node->next;
+    }
+
+    return arr;
+}
+
+
+// Returns the number of elements in the list.
+unsigned int list_size(LIST *list){
+    unsigned int i = 0;
+    NODE *node = list->head; 
+    while(node != NULL){
+        i++;
+        node = node->next;
+    }
+
+    return i;
+}
+
+
+// Returns true if empty, false otherwise.
+bool list_is_empty(LIST *list){
+    if (list->head == NULL && list->tail == NULL) {
+        return true;
+    }
+    return false;
+}
+
+
+// destroy a list: CANNOT be called with list_free
+// Deletes all nodes, then list iself. Will create memory leak if data is initialized within nodes.
+void list_destroy(LIST *list){
+    NODE *prev; 
+    NODE *node = list->head; 
+
+    while(node != NULL){
+        prev = node;
+        node = node->next;
+        free(prev);
+    }
     
+    free(list);
+}
+
+
+// free list: CANNOT be called after list_destroy
+// Param: List, and a function to free a node's internal data
+// Deletes all data from nodes using provided function, then deletes node itself, then list
+void list_free(LIST *list, void (*free_data)(void *data)){
+    NODE *prev = list->head; 
+    NODE *node = list->head; 
+
+    while(node != NULL){
+        prev = node;
+        node = node->next;
+        free_data(prev->data); 
+        free(prev);
+    }
+    
+    free(list); 
 }
--- lab04/test_list.c
+++ lab04/test_list.c
@@ -3,71 +3,279 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <cmocka.h>
+#include <assert.h>
 
 #include "list_internal.h"
 
-struct point {
+// prevents compiler warning about unused parameters
+#define UNUSED(x) (void)(x)
+
+// The real (not mocked) free function.
+// Call this function if you do a malloc in a
+// a unit test function.
+void __real_free(void *ptr);
+
+void __wrap_free(void *ptr);
+
+/**
+ * @brief Mock the free() function during unit testing.
+ * You need to link with the following options for the 
+ * __wrap_free() function to be called instead of the free()
+ * function: -Wl,--wrap=free 
+ * @param ptr The pointer to memory to be freed.
+ */
+void __wrap_free(void *ptr) {
+    // This enables checking that the argument passed to the free
+    // function is the argument you expected to be passed.
+    check_expected_ptr(ptr);
+}
+
+// This struct used as the data to store in the linked list.
+typedef struct {
     int x;
     int y;
-};
+} DATA;
+
+// Linked list nodes and data used for testing.
+// More nodes and data can be added if necessary.
+LIST list;
+NODE node0, node1, node2, node3;
+NODE *const nodes[] = {&node0, &node1, &node2, &node3};
+DATA data0, data1, data2, data3;
+DATA *const elements[] = {&data0, &data1, &data2, &data3};
+
+/**
+ * @brief Resets the linked list test data.
+ */
+void reset_list() {
+    list.head = NULL;
+    list.tail = NULL;
+    for (unsigned long i = 0; i < sizeof(nodes) / sizeof(nodes[0]); ++i) {
+        nodes[i]->data = NULL;
+        nodes[i]->next = NULL;
+    }
+}
+
+/**
+ * Setup the linked list test data. The node_count
+ * is the number of nodes to add to the list. Each node
+ * points to a data element.
+ */
+void setup_list(int node_count) {
+    reset_list();
+    if (node_count) {
+        list.head = nodes[0];
+        list.tail = nodes[0];
+        nodes[0]->data = elements[0];
+        nodes[0]->next = NULL;
+        for (int i = 1; i < node_count; ++i) {
+            nodes[i - 1]->next = nodes[i];
+            nodes[i]->data = elements[i];
+        }
+        list.tail = nodes[node_count - 1];
+    }
+}
 
 void test_list_create(void **state) {
+    UNUSED(state);
     LIST *list = list_create();
 
     assert_non_null(list);
     assert_null(list->head);
     assert_null(list->tail);
 
-    free(list);
+    __real_free(list);
 }
 
+
+//APPEND tests
+
 void test_list_append_when_list_is_empty(void **state) {
-    // AAA arrange, act, assert
-    //arrange
-    LIST list;
-    list.head = NULL;
-    list.tail = NULL;
-    struct point element;
+    UNUSED(state);
+    // arrange
+    reset_list();
 
-    //act
-    list_append(&list, &element);
+    // act
+    list_append(&list, elements[0]);
 
-    //assert
+    // assert
     assert_non_null(list.head);
     assert_non_null(list.tail);
     assert_ptr_equal(list.head, list.tail);
-    assert_ptr_equal(list.head->data, &element);
+    assert_ptr_equal(list.head->data, elements[0]);
     assert_null(list.head->next);
 }
 
 void test_list_append_when_list_has_one_node(void **state) {
-    // arrange
-    LIST list;
-    NODE node;
-    struct point element;
-    list.head = &node;
-    list.tail = &node;
-    node.data = &element;
-    node.next = NULL;
-    struct point element2;
-    
-    // act
-    list_append(&list, &element2);
+    UNUSED(state);
+    setup_list(1);
+    DATA new_data;
 
-    // assert
-    assert_ptr_equal(list.head, &node);
-    assert_ptr_equal(list.head->data, &element);
+    list_append(&list, &new_data);
+
+    assert_ptr_equal(list.head, nodes[0]);
+    assert_ptr_equal(list.head->data, elements[0]);
     assert_ptr_equal(list.head->next, list.tail);
-    assert_ptr_equal(list.tail->data, &element2);
+    assert_ptr_equal(list.tail->data, &new_data);
     assert_null(list.tail->next);
 }
 
+
+void test_list_insert_at_when_list_is_empty(void **state) {
+    UNUSED(state);
+    reset_list();
+
+    list_insert_at(&list, elements[0], 1);
+
+    assert_non_null(list.head);
+    assert_non_null(list.tail);
+    assert_ptr_equal(list.head, list.tail);
+    assert_ptr_equal(list.head->data, elements[0]);
+    assert_null(list.head->next);
+}
+
+
+void test_list_insert_at_when_list_has_one_node(void **state) {
+    UNUSED(state);
+    setup_list(1);
+    DATA new_data;
+
+    list_insert_at(&list, &new_data, 1);
+
+    assert_ptr_equal(list.head, nodes[0]);
+    assert_ptr_equal(list.head->data, elements[0]);
+    assert_ptr_equal(list.head->next, list.tail);
+    assert_ptr_equal(list.tail->data, &new_data);
+    assert_null(list.tail->next);
+}
+
+//SIZE test
+
+void test_list_size_when_list_is_empty(void **state) {
+    UNUSED(state);
+    reset_list();
+
+    int res = list_size(&list);
+
+    assert(res == 0);
+}
+
+void test_list_size_when_list_is_not_empty(void **state) {
+    UNUSED(state);
+    int size = 3;
+    setup_list(3);
+
+    int res = list_size(&list);
+
+    assert(res == size);
+}
+
+
+//IS_EMPTY test
+void test_list_is_empty_when_list_is_empty(void **state) {
+    UNUSED(state);
+    setup_list(1);
+    reset_list();
+
+    bool res = list_is_empty(&list);
+
+    assert_null(list.head);
+    assert_null(list.tail);
+    assert(res);
+}
+
+void test_list_is_empty_when_list_is_not_empty(void **state) {
+    UNUSED(state);
+    setup_list(4);
+
+    bool res = list_is_empty(&list);
+
+    assert_non_null(list.head);
+    assert_non_null(list.tail);
+    assert(res == false);
+}
+
+
+//REMOVE tests
+
+void test_list_remove_when_list_has_one_node(void **state) {
+    UNUSED(state);
+    setup_list(1);
+
+    expect_value(__wrap_free, ptr, nodes[0]);
+    list_remove(&list, elements[0]);
+
+    assert_null(list.head);
+    assert_null(list.tail);
+}
+
+
+void test_list_remove_when_list_has_more_than_1_node(void **state) {
+    UNUSED(state);
+    setup_list(3);
+
+    expect_value(__wrap_free, ptr, nodes[1]);
+    list_remove(&list, elements[1]);
+
+    assert_ptr_equal(list.head, nodes[0]);
+    assert_ptr_equal(list.tail, nodes[2]);
+}
+
+
+void test_list_remove_at_when_list_has_one_node(void **state) {
+    UNUSED(state);
+    setup_list(1);
+
+    expect_value(__wrap_free, ptr, nodes[0]);
+    void *el_removed = list_remove_at(&list, 0);
+
+    assert_ptr_equal(el_removed, elements[0]);
+    assert_null(list.head);
+    assert_null(list.tail);
+}
+
+
+void test_list_remove_at_when_list_has_more_than_1_node(void **state) {
+    UNUSED(state);
+    setup_list(3);
+
+    expect_value(__wrap_free, ptr, nodes[1]);
+    void *el_removed = list_remove_at(&list, 1);
+
+    assert_ptr_equal(el_removed, elements[1]);
+    assert_ptr_equal(list.head, nodes[0]);
+    assert_ptr_equal(list.tail, nodes[2]);
+}
+
+
+// Destroy Test
+
+void test_list_destroy(void **state) {
+    UNUSED(state);
+    setup_list(2);
+
+    expect_value(__wrap_free, ptr, nodes[0]);
+    expect_value(__wrap_free, ptr, nodes[1]);
+    expect_value(__wrap_free, ptr, &list);
+
+    list_destroy(&list);
+}
 
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_list_create),
         cmocka_unit_test(test_list_append_when_list_is_empty),
-        cmocka_unit_test(test_list_append_when_list_has_one_node)
-    };
+        cmocka_unit_test(test_list_append_when_list_has_one_node),
+        cmocka_unit_test(test_list_insert_at_when_list_is_empty),
+        cmocka_unit_test(test_list_insert_at_when_list_has_one_node),
+        cmocka_unit_test(test_list_remove_when_list_has_one_node),
+        cmocka_unit_test(test_list_remove_when_list_has_more_than_1_node),
+        cmocka_unit_test(test_list_remove_at_when_list_has_one_node),
+        cmocka_unit_test(test_list_remove_at_when_list_has_more_than_1_node),
+        cmocka_unit_test(test_list_size_when_list_is_not_empty),
+        cmocka_unit_test(test_list_size_when_list_is_empty),
+        cmocka_unit_test(test_list_is_empty_when_list_is_empty),
+        cmocka_unit_test(test_list_is_empty_when_list_is_not_empty),
+        cmocka_unit_test(test_list_destroy)};
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
