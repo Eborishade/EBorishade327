88d98b7a8f5e26d0799c22ac914f7b9703497199
Emmanuel <eborishade@gmail.com>
2023-04-21 12:18:17 -0500

    assign6 notes

--- assign6/exec_env.c
+++ assign6/exec_env.c
@@ -13,35 +13,53 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 
-    //create environmental variables to pass to execd program
-    char *env_vars[6];
+  //create environmental variables to pass to execd program
+  char *env_vars[6];
 
-    env_vars[0] = calloc(100, sizeof(char));
-    strcpy(env_vars[0], "HOME=");
-    strcat(env_vars[0], getenv("HOME")); 
-    
-    env_vars[1] = "PATH=/usr/bin";
+  /*Better way to define env_vars
+    #define SHELL
+    #define HOME
+    #define PWD
+    #define EXECD_PGM "EXECD_PGM"
+    #define PATH_VALUE "usr/bin"
+    ...
+    env_vars[0] = make_env_var(HOME, getenv(HOME));
+    env_vars[1] = make_env_var(PATH, getenv(PATH_VALUE));
+    env_vars[2] = make_env_var(SHELL, getenv(SHELL));
+    env_vars[3] = argv[2]
+    env_vars[4] = make_env_var(PWD, getenv(PWD));
+  */
 
-    env_vars[2] = calloc(100, sizeof(char));
-    strcpy(env_vars[2], "SHELL=");
-    strcat(env_vars[2], getenv("SHELL"));
-    
-    env_vars[3] = argv[2];
+  env_vars[0] = calloc(100, sizeof(char));
+  strcpy(env_vars[0], "HOME=");
+  strcat(env_vars[0], getenv("HOME")); 
+  
+  env_vars[1] = "PATH=/usr/bin";
 
-    env_vars[4] = calloc(100, sizeof(char));
-    strcpy(env_vars[4], "PWD=");
-    strcat(env_vars[4], getenv("PWD"));
-    env_vars[5] = NULL;
-    
+  env_vars[2] = calloc(100, sizeof(char));
+  strcpy(env_vars[2], "SHELL=");
+  strcat(env_vars[2], getenv("SHELL"));
+  
+  env_vars[3] = argv[2];
 
-    //get path to program and new name to run under
-    char* pgm_path = argv[1];
-    char* pgm_name = argv[2];
+  env_vars[4] = calloc(100, sizeof(char));
+  strcpy(env_vars[4], "PWD=");
+  strcat(env_vars[4], getenv("PWD"));
+  env_vars[5] = NULL;
+  
 
-    if(execle(pgm_path, pgm_name, NULL, env_vars) == -1){
-		perror("execle Failed");
-		exit(EXIT_FAILURE);
-	}
+  //get path to program and new name to run under
+  char* pgm_path = argv[1];
+  char* pgm_name = argv[2];
 
-	exit(EXIT_SUCCESS);
+  if(execle(pgm_path, pgm_name, NULL, env_vars) == -1){
+    perror("execle Failed");
+    //free space on failure then exit
+    for (int i = 0; i < (sizeof(env_vars) / sizeof(env_vars[0])); i++){
+      free(env_vars[x])
+    }
+    exit(EXIT_FAILURE);
+  }
+
+  exit(EXIT_SUCCESS); //unreachable
 }
